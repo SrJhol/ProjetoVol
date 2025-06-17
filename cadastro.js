f4bed996ea4d2e4e6315047dd1e43fbe4cb38bb0
SrJhol <mendes.jhonatan039@gmail.com>
2025-06-17 19:51:26 -0300

    feat: adicionar formatação automática para campos de CPF e telefone
    pontuações ao digitar telefone e CPF automaticamente  no cadastro facilitando o acesso

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,18 @@
+document.getElementById("cpf").addEventListener("input", function () {
+    let value = this.value.replace(/\D/g, "");
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    this.value = value;
+  });
+
+  document.getElementById("telefone").addEventListener("input", function () {
+    let value = this.value.replace(/\D/g, "");
+    value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
+    value = value.replace(/(\d{5})(\d{1,4})$/, "$1-$2");
+    this.value = value;
+  });
+
 document.getElementById('cep').addEventListener('blur', function() {
     fetch(`https://viacep.com.br/ws/${this.value}/json/`)
         .then(response => response.json())
@@ -33,3 +48,4 @@ document.getElementById('form').addEventListener('submit', function(e) {
     alert("Cadastrado com sucesso!!!");
     this.reset();
 });
+
